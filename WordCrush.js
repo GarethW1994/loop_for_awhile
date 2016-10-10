0e25874b7d1d1d3f5befbd92ae35f05dbf411aff
GarethW1994 <williams.gareth0@gmail.com>
2016-10-10 15:56:58 +0200

    Adding file - Not complete

---
+++ WordCrush.js
@@ -0,0 +1,21 @@
+function longestWord(sentence) {
+    var  splitSentence = sentence.split(" ")
+
+    var keepSentence = [];
+    var sentenceNum = [];
+    var longestWord = [];
+
+      for (var y = 0; y < splitSentence.length; y++) {
+        sentenceNum.push(parseInt(splitSentence[y].length));
+
+        longestWord.push()
+      }
+
+
+
+      console.log(check);
+    //var wordsNum = parseInt(keepSentence);
+
+}
+
+longestWord('I am a ninja');
